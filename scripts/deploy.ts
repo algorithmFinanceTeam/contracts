896332c758001a656101c119cdd333a85d388edc
HaMiD <smileman926@hotmail.com>
2022-03-11 17:19:37 +0330

    invest

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,6 +10,23 @@ async function main() {
   await wmatic.deployed();
 
   console.log("wmatic deployed to:", wmatic.address);
+
+
+  const Funding = await hre.ethers.getContractFactory("Funding");
+  const funding = await Funding.deploy(wmatic.address);
+
+  await funding.deployed();
+
+  console.log("funding deployed to:", funding.address);
+
+  const NRT = await hre.ethers.getContractFactory("NRT");
+  const nrt = await NRT.deploy(wmatic.address,'PRX',9);
+
+  await nrt.deployed();
+
+  console.log("nrt deployed to:", nrt.address);
+
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
